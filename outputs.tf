71a908f92edd6166cd15c420c135544bd46e712e
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2021-12-07 00:11:08 +0800

    Add more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
+output "name" {
+  value = hcloud_load_balancer.load_balancer.name
+}
+
+output "region" {
+  value = hcloud_load_balancer.load_balancer.network_zone
+}
+
+output "ids" {
+  value = hcloud_load_balancer.load_balancer.id
+}
+
 output "ips" {
-  value =  hcloud_load_balancer.load_balancer.ipv4
+  value = hcloud_load_balancer.load_balancer.ipv4
 }
